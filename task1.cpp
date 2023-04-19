cd4fb54bbf3bb5b95f4148f436b0f4a790a43093
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-04-19 16:09:52 +1100

    'first'

--- task1.cpp
+++ task1.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <vector>
+
+void swapvec(std::vector<int>& a, b[]) {
+
+	for (int i = 0; i < a.size(); ++i) {
+
+		a[i] = a[i] + b[i];
+		b[i] = a[i] - b[i];
+		a[i] = a[i] - b[i];
+
+	}
+
+}
 
 int main() {
 
+	std::vector<int> a = { 1,2,3,4 };
 
+	int b[] = { 2,4,6,8 };
 
+	swapvec(a, b);
+
+	for (int i = 0; i < 4; ++i) {
+
+		std::cout << a[i];
+
+		std::cout << std::endl;
+
+	}
+
+		
 }
